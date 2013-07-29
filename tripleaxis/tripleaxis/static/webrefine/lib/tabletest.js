c2c4f251934fe8bd5c9a8b39222012dadcc7dac9
MrComputer <alexander@alexander-OptiPlex-745.(none)>
2013-07-29 09:34:57 -0400

    got rid of extra stuff

--- tripleaxis/tripleaxis/static/webrefine/lib/tabletest.js
+++ tripleaxis/tripleaxis/static/webrefine/lib/tabletest.js
@@ -959,24 +959,7 @@ Ext.onReady(function () {
 	item["h"]=h;
 	item["k"]=k;
 	item["l"]=l;
-        Number.prototype.noExponents= function(){
-	    var data= String(this).split(/[eE]/);
-	    if(data.length== 1) return data[0]; 
-
-	    var  z= '', sign= this<0? '-':'',
-	    str= data[0].replace('.', ''),
-	    mag= Number(data[1])+ 1;
-
-	    if(mag<0){
-		z= sign + '0.';
-		while(mag++) z += '0';
-		return z + str.replace(/^\-/,'');
-	    }
-	    mag -= str.length;  
-	    while(mag--) z += '0';
-	    return str + z;
-	}
-	item["|F|"] = f.noExponents();
+	item["|F|"] = f;
 	item['2Ѳ'] = twoTheta;
 
 	var itemModel = Ext.create('resultsModel', item);
